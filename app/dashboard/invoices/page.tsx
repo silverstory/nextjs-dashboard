9df5d2f5deda4d86244211ccd79d63988021990f
Borge <borge@milkyway.com>
2024-01-29 13:52:07 +0800

    completed the course.

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -7,6 +7,16 @@ import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
 
+import { Metadata } from 'next';
+
+// export const metadata: Metadata = {
+//   title: 'Invoices | Menu + Recipe',
+// };
+
+export const metadata: Metadata = {
+  title: 'Invoices',
+};
+
 export default async function Page({
   searchParams,
 }: {
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,22 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
- 
+import { Metadata } from 'next';
+
+// export const metadata: Metadata = {
+//   title: 'Menu + Recipe',
+//   description: 'The official Menu + Recipe Dashboard, 2k24.',
+//   metadataBase: new URL('https://menu.op-proper.gov.ph'),
+// };
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Menu + Recipe',
+    default: 'Menu + Recipe',
+  },
+  description: 'The official Menu + Recipe Dashboard 2k24.',
+  metadataBase: new URL('https://menu.op-proper.gov.ph'),
+};
+
 export default function RootLayout({
   children,
 }: {
